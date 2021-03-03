6c499c8acdcf0814b2a9716a74cba5a8b015141a
Cyprian <cypchg@gmail.com>
2021-03-03 17:17:30 +0800

    update 2 - 03/03/2021

---
+++ global_assets/js/main/formDOM.js
@@ -0,0 +1,27 @@
+function fxBumiputera() {
+  var checkBox = document.getElementById("boxBumiputera");
+  var text = document.getElementById("bumiputeraForm");
+  if (checkBox.checked == true) {
+    bumiputeraForm.style.display = "block";
+  } else {
+    bumiputeraForm.style.display = "none";
+  }
+}
+function fxNonBumiputera() {
+  var checkBox = document.getElementById("boxNonBumiputera");
+  var text = document.getElementById("nonBumiputeraForm");
+  if (checkBox.checked == true) {
+    nonBumiputeraForm.style.display = "block";
+  } else {
+    nonBumiputeraForm.style.display = "none";
+  }
+}
+function fxForeigner() {
+  var checkBox = document.getElementById("boxForeigner");
+  var text = document.getElementById("foreignerForm");
+  if (checkBox.checked == true) {
+    foreignerForm.style.display = "block";
+  } else {
+    foreignerForm.style.display = "none";
+  }
+}
